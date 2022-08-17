8b3581735697e6cfcd4325907af90ad2c58698b5
Srinjan Ghosh <srinjanghosh11@gmail.com>
2022-08-18 01:16:25 +0530

    base structure markup

--- src/App.js
+++ src/App.js
@@ -2,8 +2,42 @@ import './App.css'
 
 const App = () => {
 	return (
-		<div className='App'>
-			<h1>Hello World</h1>
+		<div className='categories-container'>
+			<div className='category-container'>
+				{/* <img src="" alt="" /> */}
+				<div className='category-body-container'>
+					<h2>Hats</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+			<div className='category-container'>
+				{/* <img src="" alt="" /> */}
+				<div className='category-body-container'>
+					<h2>Jackets</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+			<div className='category-container'>
+				{/* <img src="" alt="" /> */}
+				<div className='category-body-container'>
+					<h2>Sneakers</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+			<div className='category-container'>
+				{/* <img src="" alt="" /> */}
+				<div className='category-body-container'>
+					<h2>Women</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
+			<div className='category-container'>
+				{/* <img src="" alt="" /> */}
+				<div className='category-body-container'>
+					<h2>Men</h2>
+					<p>Shop Now</p>
+				</div>
+			</div>
 		</div>
 	)
 }
