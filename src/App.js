c72d3a98b67241533679f1a7211fba9f3b0b35e7
Srinjan Ghosh <srinjanghosh11@gmail.com>
2022-08-18 13:57:48 +0530

    navigation bar styling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { Routes, Route } from 'react-router'
 import Home from './routes/home/home.component'
 import Navigation from './routes/navigation/navigation.component'
 
+const Shop = () => {
+	return <h1>This is the shop page</h1>
+}
+
 const App = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Navigation />}>
 				<Route index element={<Home />} />
+				<Route path='shop' element={<Shop />} />
 			</Route>
 		</Routes>
 	)
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,16 +1,28 @@
-import { Outlet } from 'react-router'
+import { Fragment } from 'react'
+import { Outlet, Link } from 'react-router-dom'
+
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
+
+import './navigation.styles.scss'
 
 const Navigation = () => {
 	return (
-		<div>
-			<div>
-				<h1>Navigation Bar</h1>
+		<Fragment>
+			<div className='navigation'>
+				<Link className='logo-container' to='/'>
+					<CrownLogo className='logo' />
+				</Link>
+				<div className='nav-links-container'>
+					<Link className='nav-link' to='/shop'>
+						SHOP
+					</Link>
+				</div>
 			</div>
 			{/* all components will be renderd below the top component Navigation 
-          the placing of outlet determines the order in nested routing
-      */}
+                the placing of outlet determines the order in nested routing
+            */}
 			<Outlet />
-		</div>
+		</Fragment>
 	)
 }
 
