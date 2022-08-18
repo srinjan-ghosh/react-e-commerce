aa3fdd28a0ea7d3e515ccc2683c95c8d33115686
Srinjan Ghosh <srinjanghosh11@gmail.com>
2022-08-19 00:41:38 +0530

    sign in form component, showing the signin form on the auth page

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -4,18 +4,15 @@
 // import {
 // 	signInWithGoogleRedirect,
 // } from '../../utils/firebase/firebase.utils'
+import SignInForm from '../../components/sign-in-form/sign-in-form.component'
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
-import {
-	signInWithGooglePopup,
-	createUserDocumentFromAuth,
-} from '../../utils/firebase/firebase.utils'
 
 const Authentication = () => {
-	const logGoogleUser = async () => {
-		const { user } = await signInWithGooglePopup()
-		// console.log(response)
-		const docRef = await createUserDocumentFromAuth(user)
-	}
+	// const logGoogleUser = async () => {
+	// 	const { user } = await signInWithGooglePopup()
+	// 	// console.log(response)
+	// 	const docRef = await createUserDocumentFromAuth(user)
+	// }
 
 	// const logGoogleRedirectUser = async () => {
 	// 	const response = await signInWithGoogleRedirect()
@@ -40,7 +37,7 @@ const Authentication = () => {
 	return (
 		<div>
 			<h1>Sign In Page</h1>
-			<button onClick={logGoogleUser}>Sign in with google</button>
+			<SignInForm />
 			{/* <button onClick={logGoogleRedirectUser}>
 				Sign in with google redirect
 			</button> */}
