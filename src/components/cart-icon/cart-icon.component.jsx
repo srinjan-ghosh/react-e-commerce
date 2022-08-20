e0061af8fe46e477cb6b88fd7be7db5a1eb18ad7
Srinjan Ghosh <srinjanghosh11@gmail.com>
2022-08-21 00:05:08 +0530

    showing total cart items

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,14 +7,23 @@ import { CartContext } from '../../contexts/cart.context'
 import './cart-icon.styles.scss'
 
 const CartIcon = () => {
-	const { isCartOpen, setIsCartOpen } = useContext(CartContext)
+	const { isCartOpen, setIsCartOpen, cartItems, cartItemCount } =
+		useContext(CartContext)
+
+	// const getQuantity = () => {
+	// 	let qty = 0
+	// 	cartItems.forEach((cartItem) => {
+	// 		qty += cartItem.quantity
+	// 	})
+	// 	return qty
+	// }
 
 	const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
 
 	return (
 		<div className='cart-icon-container' onClick={toggleIsCartOpen}>
 			<ShoppingIcon className='shopping-icon' />
-			<span className='item-count'>0</span>
+			<span className='item-count'>{cartItemCount}</span>
 		</div>
 	)
 }
