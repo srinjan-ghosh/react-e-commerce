1916adfb44367e5a0634109b6bc040814dc39aeb
Srinjan Ghosh <srinjanghosh11@gmail.com>
2022-08-30 00:18:26 +0530

    displaying all products

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -3,17 +3,19 @@ import { createContext, useState, useEffect } from 'react'
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils'
 // not needed as data is already in the database -> import SHOP_DATA from '../shop-data'
 
-export const ProductsContext = createContext({
-	products: [],
+// refactoring from products to categories
+export const CategoriesContext = createContext({
+	categories: {},
 })
 
-export const ProductsProvider = ({ children }) => {
-	const [products, setProducts] = useState([])
+export const CategoriesProvider = ({ children }) => {
+	const [categoriesMap, setCategoriesMap] = useState({})
 
 	useEffect(() => {
 		const getCategoriesMap = async () => {
 			const categoryMap = await getCategoriesAndDocuments()
-			console.log(categoryMap)
+			// console.log(categoryMap)
+			setCategoriesMap(categoryMap)
 		}
 
 		getCategoriesMap()
@@ -23,10 +25,10 @@ export const ProductsProvider = ({ children }) => {
 	// useEffect(() => {
 	// 	addCollectionAndDocuments('categories', SHOP_DATA)
 	// }, [])
-	const value = { products }
+	const value = { categoriesMap }
 	return (
-		<ProductsContext.Provider value={value}>
+		<CategoriesContext.Provider value={value}>
 			{children}
-		</ProductsContext.Provider>
+		</CategoriesContext.Provider>
 	)
 }
