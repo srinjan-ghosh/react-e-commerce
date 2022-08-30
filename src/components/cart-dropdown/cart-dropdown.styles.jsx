3b565fd48c369fe7f9183a72d9bdf26fd744814f
Srinjan Ghosh <srinjanghosh11@gmail.com>
2022-08-31 01:42:38 +0530

    css changes in cart dropdown

--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -8,31 +8,31 @@ import {
 
 export const CartDropdownContainer = styled.div`
 	position: absolute;
-	width: 240px;
-	height: 340px;
+	width: 300px;
+	height: 400px;
 	display: flex;
 	flex-direction: column;
 	padding: 20px;
 	border: 1px solid black;
 	background-color: white;
-	top: 90px;
+	top: 80px;
 	right: 40px;
 	z-index: 5;
 	${BaseButton},
 	${GoogleSignInButton},
-  ${InvertedButton} {
+	${InvertedButton} {
 		margin-top: auto;
 	}
 `
 
 export const EmptyMessage = styled.span`
 	font-size: 18px;
-	margin: 50px auto;
+	margin: auto auto;
 `
 
 export const CartItems = styled.div`
-	height: 240px;
+	height: 300px;
 	display: flex;
 	flex-direction: column;
-	overflow: scroll;
+	overflow: auto;
 `
